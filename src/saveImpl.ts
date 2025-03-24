657b92f355c40d9f85fa722b27d9052eff76089e
Narimasa Sakurai <whiteonyx@gmail.com>
2025-03-24 19:29:01 +0900

    add log

--- src/saveImpl.ts
+++ src/saveImpl.ts
@@ -19,6 +19,7 @@ export async function saveImpl(
 ): Promise<number | void> {
     let cacheId = -1;
     try {
+        core.info(process.env['ACTIONS_RESULTS_URL']??"");
         if (!utils.isCacheFeatureAvailable()) {
             return;
         }
